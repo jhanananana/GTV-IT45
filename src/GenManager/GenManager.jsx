0bdaa933b82fc4ceb6d96c0d5737cf2eef2a4189
jhanananana <jhanaptrinidad@su.edu.ph>
2025-07-22 14:20:39 +0800

    Update GenManager.jsx

--- src/GenManager/GenManager.jsx
+++ src/GenManager/GenManager.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './genmanager.css';
+import './GenManager.css';
 import Navbar from '../NavBarAndFooter/navbar.jsx'; 
 import Footer from '../NavBarAndFooter/footer.jsx'; 
 
